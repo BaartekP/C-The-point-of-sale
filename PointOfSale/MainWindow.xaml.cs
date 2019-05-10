1321a7c8eed915dfddd573155b860b37703ff3ac
bartek <bartek.praselski@outlook.com>
2019-05-10 23:35:52 +0200

    Class Product and Read file added

--- PointOfSale/MainWindow.xaml.cs
+++ PointOfSale/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data;
 
 namespace PointOfSale
 {
@@ -23,6 +25,39 @@ namespace PointOfSale
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        List<Product> spList = new List<Product>();
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            String x = this.SearchBox.Text;
+            String line;
+            int counter = 0;
+
+            if(x != null)
+            {
+                try
+                {   // Open the text file using a stream reader.
+                    StreamReader sr = new StreamReader("products.txt");
+                        // Read the stream to a string, and write the string to the console.
+                        while ((line = sr.ReadLine()) != null)
+                        {
+                            String[] parts1 = line.Split('\n');
+                            String[] parts2 = parts1[0].Split(':');
+                        foreach (String s in parts2){
+                            if (s == x) { }
+                                
+                        }
+                            counter++;
+                        }
+                }
+                catch (IOException a)
+                {
+                    Console.WriteLine("The file could not be read:");
+                    Console.WriteLine(a.Message);
+                }
+            }
+            
         }
     }
 }
